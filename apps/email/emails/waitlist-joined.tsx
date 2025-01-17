a98d4a17aabb449399e43dd18c2cc9e229bdbfb8
FindMalek <hi@findmalek.com>
2025-01-17 16:34:36 +0100

    feat: added waitlist-joined email

---
+++ apps/email/emails/waitlist-joined.tsx
@@ -0,0 +1,5 @@
+import { WaitlistJoinedTemplate } from "@undrstnd/email/templates/waitlist-joined"
+
+const ExampleWaitlistJoinedEmail = () => <WaitlistJoinedTemplate />
+
+export default ExampleWaitlistJoinedEmail
