b91a39c9d028b47ed63fc5adb26062eca69736d9
FindMalek <hi@findmalek.com>
2025-01-17 15:06:00 +0100

    fix(email): resolved '.next' not found

--- apps/waitlist/actions/waitlist.ts
+++ apps/waitlist/actions/waitlist.ts
@@ -3,15 +3,13 @@
 import { database } from "@undrstnd/database"
 
 export async function addWaitlist(email: string) {
-  console.log("Adding email to waitlist", email)
-  const x = await database.waitlist.create({
+  return await database.waitlist.create({
     data: {
       email,
       country: "fr",
       ipAddress: "0.",
     },
   })
-  console.log("Added email to waitlist", x)
 }
 
 export async function isOnWaitlist(email: string) {
