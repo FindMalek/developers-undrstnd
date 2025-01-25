12c62d9ef3e131caff7bfce7b7cecb2f211e6544
FindMalek <hi@findmalek.com>
2025-01-25 17:32:08 +0100

    fix: resolved 'NEXT_PUBLIC'

--- packages/observability/log.ts
+++ packages/observability/log.ts
@@ -3,7 +3,7 @@ import { Logger } from "@logtail/next"
 import { keys } from "./keys"
 
 const logger = new Logger({
-  token: keys().BETTERSTACK_API_KEY,
+  token: keys().NEXT_PUBLIC_BETTERSTACK_API_KEY,
 })
 
 export const log = process.env.NODE_ENV === "production" ? logger : console
