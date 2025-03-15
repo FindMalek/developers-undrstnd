28cffe9cca33e46ea0afc0ada2b6344ba1a328d6
FindMalek <hi@findmalek.com>
2025-03-15 03:22:37 +0100

    chore: minor changes in 'footer'

--- packages/email/components/footer.tsx
+++ packages/email/components/footer.tsx
@@ -15,38 +15,17 @@ export function Footer() {
   return (
     <Tailwind>
       <Container className="mx-auto flex w-full flex-col gap-y-5 rounded-lg px-7 py-5">
-        <Section className="flex items-center justify-between">
-          <Container className="flex w-full justify-around gap-x-6">
-            {site.footer.socialLinks.map((link, index) => (
-              <Link
-                key={index}
-                href={link.url}
-                className="text-muted-foreground hover:text-foreground flex h-5 w-5 items-center justify-center transition-all duration-100 ease-linear hover:underline hover:underline-offset-4"
-              >
-                <Img src={link.icon} alt="Icon"
-                  width="20"
-                  height="20"
-                />
-              </Link>
-            ))}
-          </Container>
-        </Section>
-
         <Section className="flex flex-col justify-between gap-y-5">
           <Container className="text-muted-foreground flex flex-row items-center gap-x-5">
             {site.footer.links?.map((link, index) => (
               <Link
                 key={index}
                 href={link.url}
-                className="text-muted-foreground px-2 hover:text-foreground text-[15px]/normal font-medium transition-all duration-100 ease-linear hover:underline hover:underline-offset-4"
+                className="text-muted-foreground hover:text-foreground pl-2 text-[15px]/normal font-medium transition-all duration-100 ease-linear hover:underline hover:underline-offset-4"
               >
                 {link.text}
               </Link>
             ))}
-          </Container>
-
-          <Container className="text-muted-foreground flex items-center text-sm font-medium tracking-tight">
-            <Text>{site.footer.bottomText}</Text>
           </Container>
         </Section>
 
@@ -56,6 +35,23 @@ export function Footer() {
             className="overflow-hidden font-mono text-4xl font-medium tracking-tighter"
           />
         </Container>
+
+        <Section className="flex items-center justify-between">
+          <Container className="flex w-full justify-around gap-x-6">
+            {site.footer.socialLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={link.url}
+                className="text-muted-foreground hover:text-foreground flex h-5 w-5 items-center justify-center transition-all duration-100 ease-linear hover:underline hover:underline-offset-4"
+              >
+                <Img src={link.icon} alt="Icon" width="20" height="20" />
+              </Link>
+            ))}
+          </Container>
+          <Container className="text-muted-foreground flex items-center text-sm font-medium tracking-tight">
+            <Text>{site.footer.bottomText}</Text>
+          </Container>
+        </Section>
       </Container>
     </Tailwind>
   )
